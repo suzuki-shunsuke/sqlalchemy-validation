f3357f45b4eb4c57cd6231f828e28fc890aef224
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2015-10-15 20:23:44 +0900

    Bug fixes and Rename Error classes.
    * Rename Error classes.
      * ValidationError -> BaseValidationError
      * MultiValidationError -> ValidationError
    * Change ValidationError's base class.
      * object -> BaseValidationError
    * Modify some docstrings.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sqlalchemy_validation",
-    version="0.1.0-alpha",
+    version="0.2.0a0",
     packages=["sqlalchemy_validation"],
 
     # Project uses reStructuredText, so ensure that the docutils get
